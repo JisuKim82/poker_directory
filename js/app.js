2f44e192560ceac1170ea60fe48621174352e555
Jisu Kim <jisu.kim@flatironschool.com>
2014-07-18 23:00:09 -0400

    Resolve potential issue with minification

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 var app = angular.module('myApp', []);
 
-app.controller('MyController', [$scope, $http, function($scope, $http){
+app.controller('MyController', ['$scope', '$http', function($scope, $http){
   $http.get('js/data.json').success(function(data){
     $scope.players = data;
   });
